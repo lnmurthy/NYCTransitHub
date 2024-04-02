3f3bbe46d5dcb51a269307f75a1251e6ebd7fc9b
Gabriel Bronfman <gabriel.byrne.bronfman@gmail.com>
2024-04-02 15:32:31 -0400

    added station train information

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from google.protobuf.json_format import MessageToDict
 import gtfs_realtime_pb2
 import gtfs_realtime_mta_pb2
 
+from Times import Times
+from Stations import Stations
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -21,6 +25,8 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return f'Post {post_id}'
 
+# @app.route('/station-info/<station>')
+# def show_info(station):
 
 
 @app.route("/transit")
@@ -50,3 +56,30 @@ def transit():
     
     else:
         return f"Failed to retrieve data: {response.status_code}"
+    
+@app.route('/api/train_times/')
+def train_times():
+  trains = Times().train_times
+  return jsonify(trains)
+
+@app.route('/api/train_times/<station_id>')
+def nextTrains(station_id):
+  times = Times().train_times
+  station_route = list(filter(lambda station: station['station_id'] == int(station_id), times))
+  station_route[0]['trains'] = list(filter(lambda train: train['time'] < 600, station_route[0]['trains']))
+  
+  return jsonify(station_route)
+
+@app.route('/api/station_train_info/<station_name>')
+def nextTrainsForStation(station_name):
+  times = Times().train_times
+  station_route = list(filter(lambda station: station['station_name'] == str(station_name), times))
+  if station_route:
+    station_route[0]['trains'] = list(filter(lambda train: train['time'] < 600, station_route[0]['trains']))
+  
+  return jsonify(station_route)
+
+@app.route('/api/stations/')
+def stops():
+  stations = Stations().stations
+  return jsonify(stations)
